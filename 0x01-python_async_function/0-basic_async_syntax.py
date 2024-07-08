67a50fd6de7ddb7c2a84567defa2c197990b6a17
Lexzino <alexudeze001@gmail.com>
2024-07-08 17:11:09 +0100

    task completed

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 '''Task 0's module documentation.'''
+
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
     '''Waits for random number of seconds.'''
+    
     wait_time = random.random() * max_delay
     await asyncio.sleep(wait_time)
+
     return wait_time
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """ Task 2's Module documentation """
+
 import asyncio
 import time
 
@@ -8,6 +9,7 @@ wait_n = __import__("1-concurrent_coroutines").wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     """Computes average runtime of wait_n"""
+    
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end = time.perf_counter()
