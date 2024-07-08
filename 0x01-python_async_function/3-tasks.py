8727e352e744374415470ce96724b90c7599035d
Lexzino <alexudeze001@gmail.com>
2024-07-08 11:10:09 +0100

    task completed

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,11 @@
-3-tasks.py
+#!/usr/bin/env python3
+""" Task 3's Module documentation """
+import asyncio
+import time
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """Makes an asynchronous task for wait_random"""
+    return asyncio.create_task(wait_random(max_delay))
