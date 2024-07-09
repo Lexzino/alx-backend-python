add7506bb69871f7026c3602d79b52d372a244d1
Lexzino <alexudeze001@gmail.com>
2024-07-09 13:57:00 +0100

    task completed

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,12 @@
-0-async_generator.py
+#!/usr/bin/env python3
+""" Task 0's Module documentation """
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Function generates a sequence of 10 numbers."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,1 +1,17 @@
-2-measure_runtime.py
+#!/usr/bin/env python3
+'''Task 2's module documentation.'''
+import asyncio
+import time
+from importlib import import_module as using
+
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    '''Function executes async_comprehension 4 times and
+    measures thetotal execution time.
+    '''
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
